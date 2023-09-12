e44593b997bc2c7ba3b74e79d5ba6fbc3448c4ce
JannisErhard <jannis.erhard@gmx.de>
2023-09-12 12:50:38 +0200

    coefficient optimisation works, plottig of density <dded, seems fishy, density at 0 too large

--- check.py
+++ check.py
@@ -2,38 +2,34 @@ from math import exp, pi, sqrt, sin
 import numpy as np
 import matplotlib.pyplot as plt  
 import scipy.integrate as integrate
+import time
 
 
-basis_satz = [alpha for alpha in np.logspace(-4,4,19)]
-
-c=[float(i) for i in range(1,20)]
-
-def den_norm(c):
-    return sum([i for i in c])
-
-deniy =  den_norm(c)
-for i,c_i in enumerate(c):
-   c[i] = c[i]/deniy
+def gaussian(alpha,x):
+    # keep density normalized
+    return (alpha/pi)**(3/2.)*exp(-alpha*x**2)
 
 
 def rho(c,x):
     # density as expansion in basis set 
     # keep density normalized 
-    p_norm = den_norm(c)
+    p_norm = sum(c)
     p = 0
     for i,c_i in enumerate(c):
         p += c_i/p_norm*(basis_satz[i]/pi)**(3/2.)*exp(-basis_satz[i]*x**2)
     return p
 
 
-def kinetischer_energie_integrand(c,x):
+def kinetischer_energie_integrand(f,c,x):
     # (3 \pi^2)^{3/2} \frac{3}{10} \rho^{\frac{5}{3}} Yang-Parr p.108
-    return (3*pi**2)**(2/3.)*3/10.*rho(c,x)**(5/3.)
+    return (3*pi**2)**(2/3.)*3/10.*f(c,x)**(5/3.)
 
 def kinetische_energie(c):
-    res= integrate.quad(lambda x: 4*pi*x**2*kinetischer_energie_integrand(c,x),0,np.inf)
-    print(c,res)
-    return res
+    # normal version
+    res = integrate.quad(lambda x: 4*pi*x**2*kinetischer_energie_integrand(rho,c,x),0,np.inf)
+    #print(f"{c}, {res[0]}")
+    #time.sleep(0.4)
+    return res[0]
 
 def gaussian_kernpotential_energie(c):
     # use boys function trick for coulomb energy
@@ -43,19 +39,32 @@ def gaussian_kernpotential_energie(c):
     return V_eK
 
 def total_energy(c):
-    return gaussian_kernpotential_energie(c)+kinetische_energie(c)[0]
+    norm = sum(c)
+    c = [i/norm for i in c]
+    E_tot = gaussian_kernpotential_energie(c)+kinetische_energie(c)  
+    print(E_tot)
+    return E_tot
 
+
+basis_set_size = 10
+n = basis_set_size
+basis_satz = [alpha for alpha in np.logspace(-4,4,n)]
+c=[float(i) for i in range(1,n+1)]
+norm = sum(c)
+c_0 = [i/norm for i in c]
 
 
 
 # checking if rho is defined properly 
 result = integrate.quad(lambda x: 4*pi*x**2*rho(c,x),0,np.inf)
 
+print("density integral", result)
+
 # checking if functions are defined properly 
 result = integrate.quad(lambda x: 4*pi*x**2*(basis_satz[1]/pi)**(3./2.)*exp(-basis_satz[1]*x**2),0,np.inf)
 
 # E_{TF}[\rho] = 4 \pi \int_0^{\infty} dr r**2 e_{TF}[\rho] 
-V_TF = integrate.quad(lambda x: 4*pi*x**2*kinetischer_energie_integrand(c,x),0,np.inf)
+V_TF =  kinetische_energie(c)
 
 
 
@@ -84,18 +93,29 @@ spikyness = [i for i in np.linspace(1,-4,1000)]
 spike_test=False
 if spike_test:
     for spike in spikyness:
-        #basis_satz = [alpha for alpha in np.logspace(spike+2,spike-2,19)]
         basis_satz = [alpha for alpha in np.logspace(spike+1.5,spike-1.5,19)]
         V_Ke = gaussian_kernpotential_energie(c)
         V_TF = kinetische_energie(c)
-        #integrate.quad(lambda x: 4*pi*x**2*kinetischer_energie_integrand(c,x),0,np.inf)
         print(spike, spike+8, V_Ke, V_TF[0])
 
 # optimise the basis set coeficients to find the energy minimum for Hydrogen
 from scipy.optimize import minimize
 c0 = np.array(c)
 print(total_energy(c), total_energy(c0))
-res = minimize(total_energy,c)
+bnds = ((0, 1.0),)*n
+res = minimize(total_energy,c,bounds=bnds)
 print(res.x)
 print(total_energy(res.x))
 
+norm_final = sum(res.x)
+c_f = [i/norm_final for i in res.x]
+
+
+plot = True 
+if plot:
+    r = [float(i/190) for i in range(-190,190)]
+    rho_initial = [rho(c_0,x) for x in r ]
+    rho_final = [rho(c_f,x) for x in r ]
+    #plt.plot(r,rho_initial)
+    plt.plot(r,rho_final)
+    plt.show()
